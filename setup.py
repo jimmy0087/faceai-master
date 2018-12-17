1d8cdfd4711ce2e58f10c6ae58a267a11ecd0ee7
jimmy0087 <yc281121638@gmail.com>
2018-12-17 18:46:28 +0800

    V0.3.4 add __init__ in every

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup,find_packages
 setup(name="faceai",
-      version='0.3.2',
+      version='0.3.3',
       description='Deep Learning library for applications about face.',
       url="https://github.com/jimmy0087/faceai-master",
       author='JimmyYoung',
       license='JimmyYoung',
       packages= find_packages(),
+      #packages = ['faceai','faceai/Alignment/*.py','faceai/Detection/*.py','faceai/ThrDFace/*.py'],
       zip_safe=False
       )
